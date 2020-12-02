b96a0c06d86a0c55d7a87631ff26d9aee7c80cce
Arghyadyuti Patra <40797900+arghyadyuti@users.noreply.github.com>
2020-12-02 21:24:51 +0530

    Update gcpResourceDelete.sh

--- gcpResourceDelete.sh
+++ gcpResourceDelete.sh
@@ -3,13 +3,6 @@
 export GOOGLE_APPLICATION_CREDENTIALS=/Users/arghya/zaphod-aaa.json
 
 snsTopic=`aws2 sns list-topics --profile zaphodaaa --region us-east-1 | jq '.Topics[].TopicArn'`
-IFS=$'\n'
-for i in $snsTopic
-do
-  if [[ "$i" == *"topic-turbottest4353"* ]]
-  then
-  temp="${i%\"}"
-  temp="${temp#\"}"
-  deleteRes=`aws2 sns delete-topic --topic-arn "$temp" --profile zaphodaab --region us-east-1`
-  fi
-done
+
+deleteRes=`aws2 sns delete-topic --topic-arn "$temp" --profile zaphodaab --region us-east-1`
+  
