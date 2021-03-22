90a3c3fe052c6878df1166f675fddc6159dcfa85
Arghyadyuti Patra <40797900+arghyadyuti@users.noreply.github.com>
2021-03-22 23:38:56 +0530

    Delete file

--- gcpResourceDelete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/arghya/zaphod-aaa.json
-
-snsTopic=`aws2 sns list-topics --profile zaphodaaa --region us-east-1 | jq '.Topics[].TopicArn'`
-
-deleteRes=`aws2 sns delete-topic --topic-arn "$temp" --profile zaphodaab --region us-east-1`
-  
